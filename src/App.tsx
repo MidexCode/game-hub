7285f3cdcd69c8e28234140248debbaaab45b060
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2025-06-03 19:59:21 -0700

    Building a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-const App = () => {
-  return <Button>Button</Button>;
-};
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem gridArea="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <GridItem
+        gridArea="aside"
+        bg="gold"
+        display={{ base: "none", lg: "block" }}
+      >
+        Aside
+      </GridItem>
+
+      <GridItem gridArea="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
 
 export default App;
