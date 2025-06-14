d476a51b220ec4b2ae32a937b51ae673772bac8b
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2025-06-13 19:02:33 -0700

    Handle Games with no Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,19 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
   // Return placeholder if no image is available
-  return url || "https://via.placeholder.com/600x400?text=No+Image";
+  if (!url) return noImage;
+
+  const target = "media/";
+  const index = url.indexOf(target);
+
+  // If 'media/' is not found in URL, return original URL
+  if (index === -1) return url;
+
+  const targetEndIndex = index + target.length;
+  return (
+    url.slice(0, targetEndIndex) + "crop/600/400/" + url.slice(targetEndIndex)
+  );
 };
 
 export default getCroppedImageUrl;
